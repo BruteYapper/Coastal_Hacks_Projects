6a68cafff3f655e45ca215363d4ac8b664e1f2ba
williamdual <williamdual115@gmail.com>
2022-06-11 19:04:07 -0400

    donation page, rotation, formating

--- public/behaviour.js
+++ public/behaviour.js
@@ -1,6 +1,10 @@
 let bar = document.getElementById("die-bar")
 let yearText = document.getElementById("year-num")
 let fact = document.getElementById("facts-text")
+let earth = document.getElementById("earth")
+
+let curYear = new Date().getFullYear()
+let curRotation = 0
 
 document.addEventListener("load", updateFact(1900), bar.value = 1900, updateYear());
 
@@ -16,9 +20,6 @@ function updateFact(year) {
     if (year == 1900) {
         blurb = "And so it begins...";
     }
-    else if (year == 1969 || year == 2069) {
-        blurb = "nice";
-    }
     else if (year == 2050) {
         blurb = "Due to Ocean Acidification, worldwide coral growth has been cut in half";
     }
@@ -26,7 +27,7 @@ function updateFact(year) {
         blurb = "Coastal cities are Now in Danger of Being Destroyed by the Ocean";
     }
     else {
-        if (year < new Date().getFullYear()) //if its in the past
+        if (year < curYear) //if its in the past
         {
             blurb = "By " + year + " the sea level had increased by " + ((0.06265582 * year) - 118.31796934379962).toFixed(2) + " inches since 1880";
         }
@@ -35,4 +36,10 @@ function updateFact(year) {
         }
     }
     fact.textContent = blurb;
+    rotateEarth(year)
+}
+
+function rotateEarth(year) {
+    let rotAmt = -(1900 - year) * (360 / 200);
+    earth.style.transform = "rotate(" + rotAmt + "deg)";
 }
