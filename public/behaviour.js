7ecf9421510c26503be35520cf9618714c1bafae
williamdual <williamdual115@gmail.com>
2022-06-12 01:07:14 -0400

    design, added facts, updated readme

--- public/behaviour.js
+++ public/behaviour.js
@@ -12,7 +12,12 @@ let blurb;
 let special;
 const info = {
     1900: "And so it begins...",
+    1910: "The second industrial revolution exasperates CO2 emissions",
     1915: "Glaciers all around the world start melting faster than they can grow",
+    1977: "Global Warming is identified as the chief climate risk of the next century",
+    1981: "Ronald Regan's election begins popularizing \"Climate Skepticism\"",
+    1990: "First IPCC report says world has been warming and future warming seems likely.",
+    2001: "Big Oil lobbying slows many country's anti climate change measures",
     2022: "The IPCC warns that \"Half measures are no longer an option\" if Climate risks are to be mitigated",
     2040: "During the summer months, there is no ice in the arctic",
     2050: "Due to Ocean Acidification, worldwide coral growth has been cut in half",
@@ -44,6 +49,12 @@ function updateFact(year) {
         special = false //cuz its not a special year so the popup is gone
     }
     fact.textContent = blurb;
+    if (special) {
+        fact.style.color = "red";
+    }
+    else {
+        fact.style.color = "rgb(174, 179, 209)";
+    }
     rotateEarth(year);
     raiseWater(year);
     showPopup(special);
@@ -54,14 +65,14 @@ function rotateEarth(year) {
     earth.style.transform = "rotate(" + rotAmt + "deg)";
 }
 
-function raiseWater(year){
-    let raiseAmt = -(1900 - year) * (460/200)
-    waterLevel.style.top = -raiseAmt+"px";
-    waterLevel.style.height = raiseAmt+"px";
-    if(year >= 2085){
+function raiseWater(year) {
+    let raiseAmt = -(1900 - year) * (460 / 200)
+    waterLevel.style.top = -raiseAmt + "px";
+    waterLevel.style.height = raiseAmt + "px";
+    if (year >= 2085) {
         person.src = "Imgs/person-dead.png ";
     }
-    else{
+    else {
         person.src = "Imgs/person.png";
     }
 }
