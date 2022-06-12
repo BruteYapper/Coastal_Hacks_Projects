18f87c3ce19dc9ba10453dc29c8445a6d8874ce0
BruteYapper <cmeyer8@hotmail.ca>
2022-06-12 00:38:09 -0400

    final push

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { read } = require("fs");
 const path = require("path");
+// const server = require("http").createServer(app);
+// const io = io.listen(server);
 
 
 
 const app = express();
 
-app.listen(3000);
+app.listen(80);
 
 
 app.use(express.static(path.join(__dirname, "../public")));
@@ -21,3 +23,6 @@ app.get("/donate", (req, res) => {
     res.sendFile(path.join(__dirname, "../WebPage/donate.html"));
 
 })
+
+
+// server.listen(80)
